4218dd09c2205d9eed20e2135e02d94d1cc6a20b
Saksham_Jaiswal69 <a6136593@gmail.com>
2026-07-30 10:34:49 +0530

    Create num & Strings.R

---
+++ num & Strings.R
@@ -0,0 +1,18 @@
+a<-54
+b<-"hello"
+c<-"TRUE"
+F<-as.integer(a)
+class(F)
+G<-as.character(c)
+class(G)
+print(is.numeric(a))
+print(is.integer(a))
+print(as.character(a))
+p="hello my name is Saksham Jaiswal,
+my HObbies to learn something new,  # multiline string
+my fream is to become a 'Polymath'"
+cat(p)
+paste(p) #works like print
+nchar(p)
+grep("a","äa") #1st value is for string 2nd is for variable
+grep("a","äa") #1st value is for string 2nd is for variable also string
