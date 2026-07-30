8cb12a0564901dbc0232864af64c200b2acd0976
Saksham_Jaiswal69 <a6136593@gmail.com>
2026-07-30 10:34:37 +0530

    Update if_else.R

--- if_else.R
+++ if_else.R
@@ -1,10 +1,30 @@
-marks <- 35
+#if else program
+age <- 18
+
+if(age>=18)
+{
+  print("Eligible")
+}
+# QUES-2
+marks <- 30
 
 if(marks>=40)
 {
   print("Pass")
+} else{
+    print("Fail")
 }
-else
-{
-  print("Fail")
+# QUES-3
+man <- "White"
+if(man=="black"){
+  print("he is nigga")
+  if (man =="black"){
+    print("he has more Stamina")
+  }
+} else if(man == "White"){
+  if(man =="White"){
+    print("he is gay !")
+  }
+} else{
+  print("fuck off Gay !!!")
 }
