ddae7d4a47fd851db74079fec53c59c5abfc75e7
Saksham_Jaiswal69 <a6136593@gmail.com>
2026-07-30 10:34:22 +0530

    Create Arthematic.R

---
+++ Arthematic.R
@@ -0,0 +1,17 @@
+# Create two variables a = 20 and b = 10. Find:
+
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Remainder
+# Power
+a = 25
+b=65
+print(a+b) #  + -> Addition
+print(b-a) #  - -> Subtraction
+print(a*b) #  * -> Multiplication
+print(a/b) #  / -> Division
+print(a%%b) # %% -> Modulo(remainder)
+print(a%/%b) # %/% -> Integer division
+print(b**a) # ^ or **-> Exponention
