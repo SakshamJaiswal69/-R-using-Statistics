a2677c22917d66945f7e1ec03413c5308bb2d846
Saksham_Jaiswal69 <a6136593@gmail.com>
2026-08-07 09:13:57 +0530

    Create functions.R

---
+++ functions.R
@@ -0,0 +1,22 @@
+s=81
+print(sqrt(s))
+# user defined-funtion
+greet <- function()
+{
+  print("hello Welcome to the R world")
+}
+greet()
+#user defined funtion with Argument
+square <-function(num)
+{
+  result <- num *num
+  return(result)
+}
+print(square(14))
+#user defined funtion with  2 Argument
+addition <-function(num1,num2)
+{
+  result <- num1 + num2
+  return(result)
+}
+print(addition(13,17))
